65eb85350f8257306ef719e94cc3979b31669584
MahabirGupta <89118946+MahabirGupta@users.noreply.github.com>
2021-10-14 16:26:25 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,23 +5,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter your sales figure: ");
-        int sales = Integer.parseInt(input.nextLine());
-        double commision;
-        if (sales > 10000) {
-            commision = 0.30 * sales;
-            System.out.println("Your commision is: " + commision);
-        } else if (sales>=5001 && sales<=10000){
-            commision = 0.20 * sales;
-            System.out.println("Your commision is: " + commision);
-        }
-        else if (sales>=1000 && sales<=5000){
-            commision = 0.10 * sales;
-            System.out.println("Your commision is: " + commision);
-        }
-        else {
-            System.out.println("Your sales is below $1000. You are not eligible for any commision");
+        System.out.println("Please enter your age: ");
+        int age = Integer.parseInt(input.nextLine());
+        float ticketprice;
+        float totaldiscountedprice;
+        int normalprice = 7;
+        if (age < 5) {
+            ticketprice = (float) (0.40 * normalprice);
+            float roundedFloat = (float) (Math.round(ticketprice * 100.0) / 100.0);
+            System.out.println("Please pay:$ " + roundedFloat);
+        } else if (age > 60) {
+            ticketprice = (float) (0.45 * normalprice);
+            float roundedFloat1 = (float) (Math.round(ticketprice * 100.0) / 100.0);
+            System.out.println("Please pay:$ " + roundedFloat1);
+        } else {
+            Scanner input1 = new Scanner(System.in);
+            System.out.println("How many tickets do you want to buy: ");
+            int numofpeople = Integer.parseInt(input.nextLine());
+            if (numofpeople >= 2) {
+                totaldiscountedprice = (float) (0.70 * (normalprice * numofpeople));
+                float roundedFloat2 = (float) (Math.round(totaldiscountedprice * 100.0) / 100.0);
+                System.out.println("Please pay total discounted ticket price:$ " + roundedFloat2);
+            }
         }
     }
 }
+
